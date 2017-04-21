e736914f91d5186b73ae5c7bfd977bba95cece50
lukasz-kaczmarczyk <lukasz.kaczmarczyk@mail.com>
2017-04-21 10:57:02 +0200

    Update main.c

--- main.c
+++ main.c
@@ -10,20 +10,34 @@ algorithm:
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-const char ROMAN[] = {'M','D','C','L','X','V','I'};
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 #define SIZE_BUFFER 20
 #define ROMAN_QUANTITY 7
+#define NR_OF_DATA	4
 
-char tmp[20] = "MMMLL ";
+const char ROMAN[] = {'M','D','C','L','X','V','I'};
+char tmp[SIZE_BUFFER] = "MMMLL ";
+
 
 char buffer1[SIZE_BUFFER];
 char buffer2[SIZE_BUFFER];
-char l1[] = "CCCLVII"; //357 CCC
 
-char l2[] = "CLII"; //152        DIX
+char *l1 = "CCCLVII";
+char *l2 = "CLII"; 
 
+char *l3 = "I";
+char *l4 = "I"; 
+
+char l5[] = "V";
+char l6[] = "V"; 
+
+char l7[] = "V";
+char l8[] = "X"; 
+
+char l9[] = "IV";
+char l10[] = "IX"; 
+
+
+char *data[2][NR_OF_DATA] = {0};
 
 void modify(char *value, char *buffer);
 void concatenate(char *val1, char *val2);
@@ -32,26 +46,60 @@ void compute(char *in);
 void rev(char *in);
 
 int main(int argc, char *argv[]) {
-   /*
-    printf("l1: %s\n", l1);
-    printf("l2: %s\n", l2);
-    modify(l1, buffer1);
-    modify(l2, buffer2);
-    printf("buffer1: %s\n", buffer1);
-    printf("buffer2: %s\n", buffer2);
    
-    concatenate(buffer1, buffer2);
-   
-    sort(buffer1);
-    printf("after sort %s\n", buffer1);
-    */
-   
-    compute(tmp);
-    printf(" %s\n", tmp);;
-    
-    printf(": %s\n", tmp);
-    //printf("tmp[19]: %c\n", tmp[19]);
-    return 0;
+   	//data set 1
+   	data[0][0] = l1;
+   	data[1][0] = l2;
+   	
+   	//data set 2
+   	data[0][1] = l3;
+   	data[1][1] = l4;
+   	
+	//data set 3
+   	data[0][2] = l5;
+   	data[1][2] = l6;
+   	
+   	//data set 4
+   	data[0][3] = l7;
+   	data[1][3] = l8;
+   	
+   	int i;
+   	for (i = 0; i < NR_OF_DATA; i++) {
+   		printf("number 1: %s\n", data[0][i]);
+		printf("number 2: %s\n", data[1][i]);
+		
+		modify(data[0][i], buffer1);
+		modify(data[1][i], buffer2);
+		
+		concatenate(buffer1, buffer2);
+		sort(buffer1);	   
+		compute(buffer1);
+		
+		printf(" %s + %s = %s\n\n", data[0][i], data[1][i], buffer1);
+		memset(buffer1,0,strlen(buffer1));
+		memset(buffer2,0,strlen(buffer2));
+   		
+	}
+
+	
+	/*
+	   	printf("number 1: %s\n", data[0][3]);
+		printf("number 2: %s\n", data[1][3]);
+		
+		modify(data[0][3], buffer1);
+		modify(data[1][3], buffer2);
+		
+		concatenate(buffer1, buffer2);
+		sort(buffer1);	  
+		printf("after sort operation: %s\n", buffer1); 
+		compute(buffer1);
+		
+		printf(" %s + %s = %s\n\n", data[0][3], data[1][3], buffer1);
+
+	*/
+
+
+	return 0;
 }
 
 
@@ -70,7 +118,7 @@ void modify(char *value, char *buffer)
     char *it = value;
     int i = 0, j, k = 0;
 
-    while ('M' == *it) { //put M, segmentation fault possible???
+    while ('M' == *it) { 				//put M, segmentation fault possible???
         buffer[k] = ROMAN[0];
         ++k;
         ++it;
@@ -84,7 +132,7 @@ void modify(char *value, char *buffer)
         while (ROMAN[i] == *it) {
            
             if (i % 2) {
-                while (ROMAN[i] == *it) { //put M, segmentation fault possible???
+                while (ROMAN[i] == *it) { 		//put M, segmentation fault possible???
                     buffer[k] = ROMAN[i];
                     ++k;
                     ++it;
@@ -93,20 +141,20 @@ void modify(char *value, char *buffer)
             } else {
                
        
-                if (ROMAN[i-1] == *(it+1)) { //check next
-                    for(j = 0; j < 4; j++) { //put 4x the same letter
+                if (ROMAN[i-1] == *(it+1)) { 		//check next symbol
+                    for(j = 0; j < 4; j++) { 		//put 4x the same letter
                         buffer[k] = ROMAN[i];
                         ++k;
                        
                        
                     }
                     ++it;
-                } else if (ROMAN[i-2] == *(it+1)) { //check next
+                } else if (ROMAN[i-2] == *(it+1)) { 	//check next
                     buffer[k] = ROMAN[i-1];
                    
                    
                     ++k;
-                    for(j = 0; j < 4; j++) { //put 4x the same letter
+                    for(j = 0; j < 4; j++) { 		//put 4x the same letter
                         buffer[k] = ROMAN[i];
                        
                         ++k;
@@ -123,9 +171,6 @@ void modify(char *value, char *buffer)
         ++i;
        
     }
-   
-    //buffer[k] = '0';
-   
    
 }
 
@@ -170,101 +215,103 @@ void compute(char * const in) {
 	char res[20] = {0};
 	int found = 0;
 	char *it = in, *it_res = res;
-	int i = 6, j = 1, k = 0, count = 0; //count -1 becouse it helps if not found
+	int i = 6, j = 1, k = 0, count = 0; 	
 	  
 
 	   
-	    //printf("----*it: %c\n ", *it);
+
 	
 	while (i >= 0) {
 		
-		for (;'\0' != *it; ++it);            //go to the last symbol        
+		for (;'\0' != *it; ++it);            		//go to the last symbol        
 		--it;
 	
        
-        	while ((ROMAN[i] != *it)&&(it != in)) {		//find symbol in number
-        	
+        	while ((ROMAN[i] != *it)&&(it != in)) 		//find symbol in number
         		--it;
-        		printf("while ((ROMAN[i] != *it)&&(it != in)) \n");
-		}  
 		
-		if (ROMAN[i] == *it)				//found symbol
-		{
+		if (ROMAN[i] == *it)				//found symbol, count
 			++count;
-			printf("-------found: %c\n", *it);
-		}
-			//++count;
+		
 //-------------------------------------------------------       
-    		//printf("====found -> ROMAN[i]: %c, i: %d, *it: %c\n", ROMAN[i], i, *it);
+
    
     		while (*it == *(it-1)) {    			//count the same symbol
         		count += 1;
         		--it;
-        		printf("count: %d\n", count);
-              
-        		if (it == in) {           //break if it first symbol
-        			printf("!!!it == in, break\n");
+			              
+        		if (it == in)            		//break if it first value in number
             			break;
-            		}
-       			printf("not break in counting\n");
-    		}         //the last segmentation fault???
+            		
+    		}   
        
        
-        	printf("count: %d of symbol: %c\n", count, ROMAN[i]);                        //put in reverse way
-    		if (!(i%2) && (i > 0)) {
-    	
-        	switch (count) {
-		        case 4:                    //put VI or LX or DC
-		        	*it_res = ROMAN[i-1];
-		                ++it_res;
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		        break;
-		        case 5:                    //put V or L or D
-		                *it_res = ROMAN[i-1];
-		                ++it_res;   
-		        break;
-		        case 6:                    //put IV or XL or CD
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		                *it_res = ROMAN[i-1];
-		                ++it_res;
-		        break;
-		        case 7:                    //put IIV or XXL or CCD
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		                *it_res = ROMAN[i-1];
-		                ++it_res;
-		               
-		        break;
-		        case 8:                    //put IIIV or XXXL or CCCD
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		                *it_res = ROMAN[i];
-		                ++it_res;
-		                *it_res = ROMAN[i-1];
-		                ++it_res;
-		        break;
-		        case 9:
-		                //NOT POSSIBLE?
-		        break;
-		           
-		        default:                //single, double or triple symbol
-		                for (k = 0; k < count; ++k) {
-		                	*it_res = ROMAN[i];
-		                	printf("default statement\n");
-		                	++it_res;   
-		                }
-		        }
-		} else if ((i%2) && i > 0) {			//V L D handling, to improve
+    		if (!(i%2) && (i > 0)) {			//I, X, C handling
+	        	switch (count) {
+			        case 4:                    	
+			        
+			        	if (ROMAN[i-1] == *(it-1)) {		//put XI or CX or MC	
+			                	*it_res = ROMAN[i-2];
+			                	++it_res;
+			                	*it_res = ROMAN[i];
+			                	++it_res;
+			                	--it;
+			                	*it = ' ';
+			                	
+			                	
+					} else {				
+						*it_res = ROMAN[i-1];		//put VI or LX or DC
+			                	++it_res;
+			                	*it_res = ROMAN[i];
+			                	++it_res;
+						
+					}
+
+			                
+			        break;
+			        case 5:                    	//put V or L or D
+					
+			                *it_res = ROMAN[i-1];
+			                ++it_res;   
+					
+			        break;
+			        case 6:                    	//put IV or XL or CD
+			                *it_res = ROMAN[i];
+			                ++it_res;
+			                *it_res = ROMAN[i-1];
+			                ++it_res;
+			        break;
+			        case 7:                    	//put IIV or XXL or CCD
+			                *it_res = ROMAN[i];
+			                ++it_res;
+			                *it_res = ROMAN[i];
+			                ++it_res;
+			                *it_res = ROMAN[i-1];
+			                ++it_res;
+			               
+			        break;
+			        case 8:                    	//put IIIV or XXXL or CCCD
+			                *it_res = ROMAN[i];
+			                ++it_res;
+			                *it_res = ROMAN[i];
+			                ++it_res;
+			                *it_res = ROMAN[i];
+			                ++it_res;
+			                *it_res = ROMAN[i-1];
+			                ++it_res;
+			        break;
+
+			        default:                	//single, double or triple symbol
+			                for (k = 0; k < count; ++k) {
+			                	*it_res = ROMAN[i];
+			                	++it_res;   
+			                }
+			        }
+		} else if ((i%2) && i > 0) {			//V L D handling
 			
 			if (2 == count) {
-				*it_res = ROMAN[i-1];
-				++it_res;
+				
+				*it = ROMAN[i-1];
 			} else if (1 == count){
 				*it_res = ROMAN[i];
 		               	++it_res;   
@@ -279,24 +326,24 @@ void compute(char * const in) {
 		--i;
 		
 		
-		it = in;
+		//it = in;
 		count = 0;
                
         }
-	*it_res = ' ';						//last symbol: " "
-	rev(res);
+	*it_res = ' ';						//last symbol put: " "
+	rev(res);						//reverse score
 	memcpy(in,res, 20);
 }
 
-/*
-	last of character must be: " "!!!
-*/
+/*							*
+ *	last of character must be: " " symbol		*
+ *							*/			
 void rev(char *in) {
 	char *pos2 = in;
 	char *pos1 = in;
 	char tmp;
 	
-	while (' ' != *pos2) 			//find last symbol
+	while (' ' != *pos2) 					//find last symbol
 		++pos2;
 	--pos2;
 	
@@ -308,7 +355,5 @@ void rev(char *in) {
 		++pos1;
 		--pos2;
 	}
-	
-	printf(": %s\n", in);
 	
 }
